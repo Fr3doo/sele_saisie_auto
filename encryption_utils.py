7b8524e5bf62a18a90ec23a8046c0d1a9b30f950
Fr3doo <134433826+Fr3doo@users.noreply.github.com>
2024-11-24 23:17:30 +0100

    Update encryption_utils.py update niveau de log

--- encryption_utils.py
+++ encryption_utils.py
@@ -11,7 +11,7 @@ def generer_cle_aes(TAILLE_CLE=32, log_file=None) -> bytes:
     """Génère une clé AES-256 sécurisée."""
     try:
         key = os.urandom(TAILLE_CLE)
-        write_log("Clé AES générée avec succès.", log_file, "DEBUG")
+        write_log("Clé AES générée avec succès.", log_file, "CRITICAL")
         return key
     except Exception as e:
         write_log(f"Erreur lors de la génération de la clé AES : {e}", log_file, "ERROR")
@@ -28,7 +28,7 @@ def chiffrer_donnees(donnees: str, cle: bytes, TAILLE_BLOC=128, log_file=None) -
         donnees_pad = padder.update(donnees.encode()) + padder.finalize()
         donnees_chiffrees = chiffreur.update(donnees_pad) + chiffreur.finalize()
 
-        write_log("Données chiffrées avec succès.", log_file, "DEBUG")
+        write_log("Données chiffrées avec succès.", log_file, "CRITICAL")
         return chiffre.mode.initialization_vector + donnees_chiffrees
     except Exception as e:
         write_log(f"Erreur lors du chiffrement des données : {e}", log_file, "ERROR")
@@ -40,7 +40,7 @@ def stocker_en_memoire_partagee(nom: str, donnees: bytes, log_file=None):
     try:
         memoire = shared_memory.SharedMemory(name=nom, create=True, size=len(donnees))
         memoire.buf[:len(donnees)] = donnees
-        write_log(f"Données stockées en mémoire partagée avec le nom '{nom}'.", log_file, "DEBUG")
+        write_log(f"Données stockées en mémoire partagée avec le nom '{nom}'.", log_file, "CRITICAL")
         return memoire
     except Exception as e:
         write_log(f"Erreur lors du stockage en mémoire partagée : {e}", log_file, "ERROR")
@@ -54,7 +54,7 @@ def supprimer_memoire_partagee_securisee(memoire: shared_memory.SharedMemory, lo
             memoire.buf[i] = 0
         memoire.close()
         memoire.unlink()
-        write_log("Mémoire partagée supprimée de manière sécurisée.", log_file, "INFO")
+        write_log("Mémoire partagée supprimée de manière sécurisée.", log_file, "CRITICAL")
     except Exception as e:
         write_log(f"Erreur lors de la suppression sécurisée de la mémoire partagée : {e}", log_file, "ERROR")
         raise
@@ -65,7 +65,7 @@ def recuperer_de_memoire_partagee(nom: str, taille: int, log_file=None) -> bytes
     try:
         memoire = shared_memory.SharedMemory(name=nom)
         donnees = bytes(memoire.buf[:taille])
-        write_log(f"Données récupérées depuis la mémoire partagée avec le nom '{nom}'.", log_file, "DEBUG")
+        write_log(f"Données récupérées depuis la mémoire partagée avec le nom '{nom}'.", log_file, "CRITICAL")
         return memoire, donnees
     except Exception as e:
         write_log(f"Erreur lors de la récupération depuis la mémoire partagée : {e}", log_file, "ERROR")
@@ -86,7 +86,7 @@ def dechiffrer_donnees(donnees_chiffrees: bytes, cle: bytes, TAILLE_BLOC=128, lo
         unpadder = PKCS7(TAILLE_BLOC).unpadder()
         donnees = unpadder.update(donnees_pad) + unpadder.finalize()
 
-        write_log("Données déchiffrées avec succès.", log_file, "DEBUG")
+        write_log("Données déchiffrées avec succès.", log_file, "CRITICAL")
         return donnees.decode()
     except Exception as e:
         write_log(f"Erreur lors du déchiffrement des données : {e}", log_file, "ERROR")
