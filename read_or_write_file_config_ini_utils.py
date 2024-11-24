6e1225a0803dd949ee10e96cd6510a0ec12c0cf0
Fr3doo <134433826+Fr3doo@users.noreply.github.com>
2024-11-24 11:30:00 +0100

    Update read_or_write_file_config_ini_utils.py ajout fonction write_log

--- read_or_write_file_config_ini_utils.py
+++ read_or_write_file_config_ini_utils.py
@@ -3,43 +3,43 @@ import os
 import sys
 from tkinter import messagebox
 import shutil
+from logger_utils import write_log
 
-def get_runtime_config_path():
+def get_runtime_config_path(log_file=None):
     """ Détermine le chemin du fichier `config.ini` à utiliser.
     Si le fichier n'existe pas dans le répertoire courant, copie la version embarquée.
     """
     # Chemin du fichier `config.ini` dans le répertoire courant
     current_dir_config = os.path.join(os.getcwd(), "config.ini")
-    print(f"DEBUG: {current_dir_config}")
+    write_log(f"Chemin du fichier courant : {current_dir_config}", log_file, "DEBUG")
     
     # Si PyInstaller est utilisé
     if hasattr(sys, '_MEIPASS'):
         # Chemin du fichier `config.ini` embarqué
         embedded_config = os.path.join(sys._MEIPASS, "config.ini")
-        print("DEBUG: Exécution via PyInstaller. Fichier embarqué :", embedded_config)
+        write_log(f"Exécution via PyInstaller. Fichier embarqué : {embedded_config}", log_file, "DEBUG")
         
         # Copier le fichier embarqué vers le répertoire courant si nécessaire (si absent)
         if not os.path.exists(current_dir_config):
             shutil.copy(embedded_config, current_dir_config)
-            print(f"DEBUG: Copie de {embedded_config} vers {current_dir_config}")
+            write_log(f"Copie de {embedded_config} vers {current_dir_config}", log_file, "INFO")
     else:
-        print("DEBUG: Exécution en mode script.")
+        write_log("Exécution en mode script.", log_file, "DEBUG")
     
     return current_dir_config
 
 
-def read_config_ini():
-    """ Lit et charge le fichier de configuration `config.ini` et Retourne un objet ConfigParser.
-    """
+def read_config_ini(log_file=None):
+    """ Lit et charge le fichier de configuration `config.ini` et Retourne un objet ConfigParser."""
     # Obtenir le chemin du fichier de configuration
-    config_file_ini = get_runtime_config_path()
+    config_file_ini = get_runtime_config_path(log_file=log_file)
 
     # Vérifier si le fichier existe
     if not os.path.exists(config_file_ini):
-        print(f"ERREUR: Le fichier '{config_file_ini}' est introuvable.")
+        write_log(f"Le fichier '{config_file_ini}' est introuvable.", log_file, "ERROR")
         raise FileNotFoundError(f"Le fichier de configuration '{config_file_ini}' est introuvable.")
     else:
-        print(f"DEBUG: Le fichier '{config_file_ini}' a été trouvé.")
+        write_log(f"Le fichier '{config_file_ini}' a été trouvé.", log_file, "INFO")
 
     # Initialiser ConfigParser
     config = configparser.ConfigParser()
@@ -48,49 +48,47 @@ def read_config_ini():
         # Lire le fichier avec l'encodage UTF-8
         with open(config_file_ini, 'r', encoding="utf-8") as configfile:
             config.read_file(configfile)
-            print(f"DEBUG: Le fichier de configuration '{config_file_ini}' a été lu avec succès.")
+            write_log(f"Le fichier de configuration '{config_file_ini}' a été lu avec succès.", log_file, "INFO")
     except UnicodeDecodeError as e:
-        print(f"ERREUR: Erreur d'encodage lors de la lecture du fichier '{config_file_ini}'.")
+        write_log(f"Erreur d'encodage lors de la lecture du fichier '{config_file_ini}'.", log_file, "ERROR")
         raise UnicodeDecodeError(
             f"Erreur d'encodage lors de la lecture du fichier '{config_file_ini}'. "
             f"Assurez-vous qu'il est encodé en UTF-8."
         ) from e
     except Exception as e:
-        print(f"ERREUR: Une erreur inattendue est survenue lors de la lecture du fichier '{config_file_ini}': {e}")
+        write_log(f"Une erreur inattendue est survenue lors de la lecture du fichier '{config_file_ini}': {e}", log_file, "ERROR")
         raise RuntimeError(f"Erreur lors de la lecture du fichier '{config_file_ini}': {e}") from e
 
-    print("DEBUG: Configuration initialisée avec succès.")
+    write_log("Configuration initialisée avec succès.", log_file, "INFO")
     return config
 
 
-def write_config_ini(configuration_personnel):
-    """
-    Écrit et sauvegarde les modifications dans le fichier `config.ini`.
-    """
+def write_config_ini(configuration_personnel, log_file=None):
+    """Écrit et sauvegarde les modifications dans le fichier `config.ini`."""
     # Obtenir le chemin du fichier de configuration
-    config_file_ini = get_runtime_config_path()
+    config_file_ini = get_runtime_config_path(log_file=log_file)
 
     # Vérifier si le fichier existe
     if not os.path.exists(config_file_ini):
-        print(f"ERREUR: Le fichier '{config_file_ini}' est introuvable.")
+        write_log(f"Le fichier '{config_file_ini}' est introuvable.", log_file, "ERROR")
         raise FileNotFoundError(f"Le fichier de configuration '{config_file_ini}' est introuvable.")
     else:
-        print(f"DEBUG: Le fichier '{config_file_ini}' a été trouvé.")
+        write_log(f"Le fichier '{config_file_ini}' a été trouvé.", log_file, "INFO")
 
     try:
         # Écrire dans le fichier avec l'encodage UTF-8
         with open(config_file_ini, 'w', encoding="utf-8") as configfile:
             configuration_personnel.write(configfile)
-            print(f"DEBUG: Le fichier de configuration '{config_file_ini}' a été sauvegardé avec succès.")
+            write_log(f"Le fichier de configuration '{config_file_ini}' a été sauvegardé avec succès.", log_file, "INFO")
             messagebox.showinfo("Enregistré", "Configuration sauvegardée avec succès.")
     except UnicodeDecodeError as e:
-        print(f"ERREUR: Erreur d'encodage lors de la lecture du fichier '{config_file_ini}'.")
+        write_log(f"Erreur d'encodage lors de la lecture du fichier '{config_file_ini}'.", log_file, "ERROR")
         raise UnicodeDecodeError(
             f"Erreur d'encodage lors de la lecture du fichier '{config_file_ini}'. "
             f"Assurez-vous qu'il est encodé en UTF-8."
         ) from e
     except Exception as e:
-        print(f"ERREUR: Une erreur inattendue est survenue lors de la lecture du fichier '{config_file_ini}': {e}")
+        write_log(f"Une erreur inattendue est survenue lors de la lecture du fichier '{config_file_ini}': {e}", log_file, "ERROR")
         raise RuntimeError(f"Erreur lors de la lecture du fichier '{config_file_ini}': {e}") from e
 
     return config_file_ini
